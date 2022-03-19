adf8361f31a69a4529930a71b04bfb959e0bbbc0
Konstanty <Kostu96@gmail.com>
2022-03-19 18:00:33 +0100

    WIP

--- source/main.cpp
+++ source/main.cpp
@@ -1,10 +1,38 @@
 #include "cpu.h"
 
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+
 int main()
 {
-    CPU cpu;
-    for (;;)
-        cpu.runNextInstruction();
+    if (!glfwInit()) {
+        std::cerr << "GLFW init failed!\n";
+        abort();
+    }
 
+    GLFWwindow* window = glfwCreateWindow(1024, 512, "PSX Emulator", nullptr, nullptr);
+    if (!window) {
+        std::cerr << "GLFW window creation failed!\n";
+        abort();
+    }
+
+    glfwMakeContextCurrent(window);
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        std::cerr << "GLAD loader failed!\n";
+        abort();
+    }
+
+    int width, height;
+    glfwGetFramebufferSize(window, &width, &height);
+    glViewport(0, 0, width, height);
+
+    CPU cpu;
+    while (!glfwWindowShouldClose(window))
+    {
+        cpu.runNextInstruction();
+    }
+    
+    glfwTerminate();
     return 0;
 }
---
+++ source/opengl/buffers.h
@@ -0,0 +1,30 @@
+#pragma once
+#include <cstdint>
+
+class VertexBuffer
+{
+public:
+    VertexBuffer(const void* vertices, size_t size);
+    VertexBuffer(size_t size);
+    ~VertexBuffer();
+
+    void bind() const;
+    void unbind() const;
+    void setData(const void* data, size_t size) const;
+private:
+    uint32_t m_id;
+};
+
+class IndexBuffer
+{
+public:
+    IndexBuffer(uint32_t* indices, uint32_t count);
+    ~IndexBuffer();
+
+    void bind() const;
+    void unbind() const;
+    uint32_t getCount() const { return m_count; }
+private:
+    uint32_t m_id;
+    uint32_t m_count;
+};
